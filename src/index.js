85470432f0a9f8eef58c16970cdd3f8aae1f9664
railsjack <jinnahrae@gmail.com>
2019-12-04 01:05:29 +0800

    Added minHeight

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import FastImage from 'react-native-fast-image';
-import {Image} from 'react-native'
+import { Image } from 'react-native'
 
-const API_URL = 'http://sciencesoft.at/image/latexurl/image.png?dpi=600&src=';
+const API_URL = 'http://sciencesoft.at/image/latexurl/image.png?dpi=200&src=';
 
 class Latex extends Component {
   constructor(props) {
@@ -24,9 +24,11 @@ class Latex extends Component {
 
   onLoad = (event) => {
     const { width, height } = event.nativeEvent.source;
-    this.setState({ gotSize: true, 
-      width: width * this.props.height/height, 
-      height: this.props.height });
+    this.setState({
+      gotSize: true,
+      width: height < this.props.height ? width : (width / height) * this.props.height,
+      height: height < this.props.height ? this.props.minHeight : this.props.height
+    });
     this.props.onLoad && this.props.onLoad(event);
   }
 
@@ -35,10 +37,10 @@ class Latex extends Component {
     return (latexURL ?
       <Image
         onLoad={this.onLoad}
-        style={[{resizeMode: 'contain'}, this.props.style, gotSize ? {
+        style={[{ resizeMode: 'contain' }, this.props.style, gotSize ? {
           width,
           height
-        } : {width: 1, height: 1}]}
+        } : { width: 1, height: 1 }]}
         resizeMode={FastImage.resizeMode.contain}
         source={{ uri: latexURL }} />
       : null
